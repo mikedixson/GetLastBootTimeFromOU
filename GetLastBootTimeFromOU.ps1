65d593bb752851658cf901e2ce761b0f57530845
Mike Dixson <mike-github.com@webheat.co.uk>
2018-02-13 10:19:07 +0000

    Initial commit
    Tested and working. -ou parameter must be encapsulated in quotes

---
+++ GetLastBootTimeFromOU.ps1
@@ -0,0 +1,13 @@
+﻿ param (
+    [string]$ou
+ )
+
+$computerList = Get-ADComputer -Filter * -SearchBase $ou | Select-Object -ExpandProperty Name
+foreach ($computer in $computerList) {
+    if (Test-Connection -ComputerName $computer -Quiet -Count 1){
+    gwmi win32_operatingsystem -ComputerName $computer | select csname, @{LABEL='LastBootUpTime';EXPRESSION={$_.ConverttoDateTime($_.lastbootuptime)}} 
+    } else {
+    $errors += "$computer was not reachable `n"
+    }
+    }
+write-host $errors
